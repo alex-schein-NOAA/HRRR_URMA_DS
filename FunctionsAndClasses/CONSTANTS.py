65731449a0c9d270613e75970b82bfedc6625181
alex.schein <alex.schein@noaa.gov>
2025-12-09 20:12:13 +0000

    Formatting updates

--- FunctionsAndClasses/CONSTANTS.py
+++ FunctionsAndClasses/CONSTANTS.py
@@ -56,17 +56,17 @@ class CONSTANTS():
                                          'v10m':-0.339266} }
         
         self.hrrr_stddevs_dict = {'train':{'pressurf':8493.89,
-                                         't2m':11.0624, 
-                                         'd2m':9.32916, 
-                                         'spfh2m':0.00311317, 
-                                         'u10m':3.14697, 
-                                         'v10m':3.69828}, 
+                                           't2m':11.0624, 
+                                           'd2m':9.32916, 
+                                           'spfh2m':0.00311317, 
+                                           'u10m':3.14697, 
+                                           'v10m':3.69828}, 
                                   'test':{'pressurf':8490.22,
-                                         't2m':10.9822, 
-                                         'd2m':9.14753, 
-                                         'spfh2m':0.00306908, 
-                                         'u10m':3.14104, 
-                                         'v10m':3.71392} }
+                                          't2m':10.9822, 
+                                          'd2m':9.14753, 
+                                          'spfh2m':0.00306908, 
+                                          'u10m':3.14104, 
+                                          'v10m':3.71392} }
         
         self.urma_means_dict = {'train':{'pressurf':88264.4,
                                          't2m':284.451, 
@@ -82,53 +82,53 @@ class CONSTANTS():
                                          'v10m':-0.339266} }
 
         self.urma_stddevs_dict = {'train':{'pressurf':8493.89,
-                                         't2m':11.0624, 
-                                         'd2m':9.32916, 
-                                         'spfh2m':0.00311317, 
-                                         'u10m':3.14697, 
-                                         'v10m':3.69828}, 
+                                           't2m':11.0624, 
+                                           'd2m':9.32916, 
+                                           'spfh2m':0.00311317, 
+                                           'u10m':3.14697, 
+                                           'v10m':3.69828}, 
                                   'test':{'pressurf':8490.22,
-                                         't2m':10.9822, 
-                                         'd2m':9.14753, 
-                                         'spfh2m':0.00306908, 
-                                         'u10m':3.14104, 
-                                         'v10m':3.71392} }
+                                          't2m':10.9822, 
+                                          'd2m':9.14753, 
+                                          'spfh2m':0.00306908, 
+                                          'u10m':3.14104, 
+                                          'v10m':3.71392} }
         
 
         ## Common directories 
         self.DIR_TRAIN_TEST = f"/scratch3/BMC/wrfruc/aschein/Train_Test_Files"
-        self.DIR_UNET_MAIN = f"/scratch3/BMC/wrfruc/aschein/UNet_final"
-        self.DIR_TRAINED_MODELS = f"/scratch3/BMC/wrfruc/aschein/UNet_final/Trained_models"
+        self.DIR_UNET_MAIN = f"/scratch3/BMC/wrfruc/aschein/UNet_final" #Should be os.getcwd()
+        self.DIR_TRAINED_MODELS = f"{self.DIR_UNET_MAIN}/Trained_models" #f"/scratch3/BMC/wrfruc/aschein/UNet_final/Trained_models"
         self.DIR_SMARTINIT_DATA = f"/scratch3/BMC/wrfruc/aschein/SMARTINIT_STUFF/smartinit_2024/output_files" #regridded Smartinit
         self.DIR_SMARTINIT_DATA_NDFD_GRID = f"/scratch3/BMC/wrfruc/aschein/SMARTINIT_STUFF/smartinit_2024/output_files_NDFDgrid" #old Smartinit on native grid
 
         ## Indexes for selection of patches for training
         self.PATCH_SIZE=400
 
-        ## Region indices (relative to data that has already been restricted to the intersection of HRRR and Smartinit regions)
-        self.region_keyword_indices_dict = {"Colorado":{'RESTR_ORIGIN_LAT_IDX':630,
-                                                        'RESTR_ORIGIN_LON_IDX':600,
-                                                        'RESTR_PATCH_SIZE_LAT':200,
-                                                        'RESTR_PATCH_SIZE_LON':200},
-                                            "California":{'RESTR_ORIGIN_LAT_IDX':560,
-                                                          'RESTR_ORIGIN_LON_IDX':70,
-                                                          'RESTR_PATCH_SIZE_LAT':350,
-                                                          'RESTR_PATCH_SIZE_LON':200},
-                                            "West":{'RESTR_ORIGIN_LAT_IDX':460,
-                                                    'RESTR_ORIGIN_LON_IDX':50,
-                                                    'RESTR_PATCH_SIZE_LAT':750,
-                                                    'RESTR_PATCH_SIZE_LON':750},
-                                            "Appalachia":{'RESTR_ORIGIN_LAT_IDX':480,
-                                                          'RESTR_ORIGIN_LON_IDX':1400,
-                                                          'RESTR_PATCH_SIZE_LAT':400,
-                                                          'RESTR_PATCH_SIZE_LON':330},
-                                            "Great Lakes":{'RESTR_ORIGIN_LAT_IDX':810,
-                                                           'RESTR_ORIGIN_LON_IDX':1150,
-                                                           'RESTR_PATCH_SIZE_LAT':400,
-                                                           'RESTR_PATCH_SIZE_LON':600},
-                                            "Northeast":{'RESTR_ORIGIN_LAT_IDX':770,
-                                                         'RESTR_ORIGIN_LON_IDX':1580,
-                                                         'RESTR_PATCH_SIZE_LAT':450,
-                                                         'RESTR_PATCH_SIZE_LON':450} }
+        ## Region indices (relative to data that has already been restricted to the intersection of HRRR and Smartinit regions) and other data
+        self.region_keyword_dict = {"Colorado":{'RESTR_ORIGIN_LAT_IDX':630,
+                                                'RESTR_ORIGIN_LON_IDX':600,
+                                                'RESTR_PATCH_SIZE_LAT':200,
+                                                'RESTR_PATCH_SIZE_LON':200},
+                                    "California":{'RESTR_ORIGIN_LAT_IDX':560,
+                                                  'RESTR_ORIGIN_LON_IDX':70,
+                                                  'RESTR_PATCH_SIZE_LAT':350,
+                                                  'RESTR_PATCH_SIZE_LON':200},
+                                    "West":{'RESTR_ORIGIN_LAT_IDX':460,
+                                            'RESTR_ORIGIN_LON_IDX':50,
+                                            'RESTR_PATCH_SIZE_LAT':750,
+                                            'RESTR_PATCH_SIZE_LON':750},
+                                    "Appalachia":{'RESTR_ORIGIN_LAT_IDX':480,
+                                                  'RESTR_ORIGIN_LON_IDX':1400,
+                                                  'RESTR_PATCH_SIZE_LAT':400,
+                                                  'RESTR_PATCH_SIZE_LON':330},
+                                    "Great Lakes":{'RESTR_ORIGIN_LAT_IDX':810,
+                                                   'RESTR_ORIGIN_LON_IDX':1150,
+                                                   'RESTR_PATCH_SIZE_LAT':400,
+                                                   'RESTR_PATCH_SIZE_LON':600},
+                                    "Northeast":{'RESTR_ORIGIN_LAT_IDX':770,
+                                                 'RESTR_ORIGIN_LON_IDX':1580,
+                                                 'RESTR_PATCH_SIZE_LAT':450,
+                                                 'RESTR_PATCH_SIZE_LON':450} }
 
         
